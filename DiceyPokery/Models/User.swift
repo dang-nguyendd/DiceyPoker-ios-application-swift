e41a845d4cd87a521458caefde690449d0224827
s3759957 <56671002+s3759957@users.noreply.github.com>
2022-08-26 21:12:34 +0700

    Update User.swift

--- DiceyPokery/Models/User.swift
+++ DiceyPokery/Models/User.swift
@@ -21,32 +21,3 @@ struct User: Identifiable, Codable {
     }
 }
 
-
-//@State private var highscore = UserDefaults.standard.integer(forKey: "highscore")
-//@State private var coins = 10
-//@State private var reels = [0, 0, 0]
-//
-//@State private var isChooseBet1 = false
-//@State private var isChooseBet2 = false
-//@State private var isChooseBetAll = false
-//
-//
-//@State private var showingInfoView = false
-//@State private var showGameOverModal = false
-//@State private var gameWin = false
-//@State private var gameInPlay = false
-//
-//@State private var animatingReel0 = true
-//@State private var animatingReel1 = true
-//@State private var animatingReel2 = true
-//
-////Add-in
-//var player = "default"
-//
-//@State private var gameStart = true
-//@State private var allIn = false
-//@State private var coinPot = 0
-//@State private var coinToBet = 0
-//@State private var playerHand = 0
-//@State private var botHand = 1
-//@State private var currentanimation = 0
